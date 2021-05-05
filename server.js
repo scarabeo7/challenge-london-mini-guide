5738296ce1fdd6a2839b1872169e7368b6dc867c
Chizim <garrohat@yahoo.co.uk>
2021-05-05 19:32:55 +0100

    refactored code

--- server.js
+++ server.js
@@ -10,22 +10,24 @@ const stratford = require("./data/Stratford.json");
 const harrow = require("./data/Harrow.json");
 const heathrow = require("./data/Heathrow.json");
 
-app.get("/", (req, res) =>{
-    res.send("Hello and welcome to my app")
-})
-
-app.get("/stratford/pharmacies", (req, res) => {
-  res.json(stratford.pharmacies);
+app.get("/", (req, res) => {
+  res.send("Hello and welcome to my app");
 });
 
-
-app.get("/harrow/pharmacies", (req, res) => {
-  res.json(harrow.pharmacies);
+app.get("/:city", (req, res) => {
+  const city = req.params.city.toLowerCase();
+  if (city === "stratford") {
+    res.status(200).json(stratford);
+  } else if (city === "harrow") {
+    res.status(200).json(harrow);
+  } else if (city === "heathrow") {
+    res.status(200).json(heathrow);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
-app.get("/heathrow/pharmacies", (req, res) => {
-  res.json(heathrow.pharmacies);
-});
+// app.get("/")
 
 //Check that port 4040 is not in use otherwise set it to a different port
 const PORT = process.env.PORT || 4040;
